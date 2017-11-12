c13fca8f2c1f31bfada32527544524a4187a742c
Kaho Adaniya <e175723@ie.u-ryukyu.ac.jp>
2017-11-13 08:02:03 +0900

    Report3

--- src/test/jp/ac/uryukyu/ie/e175723/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e175723/EnemyTest.java
@@ -1,16 +1,12 @@
 package jp.ac.uryukyu.ie.e175723;
 
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class EnemyTest {
-    /**
-     * 「死んでる状態では攻撃できない」ことを確認。
-     */
+class EnemyTest {
     @Test
-    public void attack() {
+    void attack() {
         int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
@@ -19,6 +15,7 @@ public class EnemyTest {
             enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
         assertEquals(heroHP, hero.hitPoint);
+
     }
 
 }
